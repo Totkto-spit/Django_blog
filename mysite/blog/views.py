7b50620fcdb4ada4a2b588759d285b75c7529704
misha <https://github.com/Totkto-spit/Django_blog.git>
2025-02-03 11:01:09 +0700

    добавление постраничной разбивки в представление списка постов

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_lists = Post.published.all()
+    paginator = Paginator(post_lists, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
